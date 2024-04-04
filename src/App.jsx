c8ab040f5bbafc65bacf3631fedb19e1a5a082ec
Catalina Fajardo <tania.fajardo01@uptc.edu.co>
2024-04-04 06:53:20 -0500

    build

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import  NavBarComponent  from "mfNavBar/NavBarComponent";
+import HomePage from "./pages/HomePage";
+import AboutPage from './pages/AboutPage';
+import CharacteresPage from "./pages/CharacteresPage";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
+
+const App = () => (
+  <BrowserRouter>
+  <NavBarComponent />
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/Home" element={<HomePage />} />
+      <Route path="/Characters" element={<CharacteresPage />} />
+      <Route path="/About" element={<AboutPage />} />
+    </Routes>
+  </BrowserRouter>
+);
+ReactDOM.createRoot(document.getElementById("app")).render(<App />);
---
+++ src/pages/CharacteresPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CharacteresPage = () => {
+    return(
+        <div>
+            Characteres Page
+        </div>
+    );
+};
+
+export default CharacteresPage;
